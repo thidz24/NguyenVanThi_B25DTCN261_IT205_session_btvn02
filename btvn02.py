1302679905316b339e1db8d3e6fbd40a062bc0b0
thidz24 <142291465+thidz24@users.noreply.github.com>
2026-05-19 00:04:16 +0700

    Add files via upload

---
+++ btvn02.py
@@ -0,0 +1,9 @@
+# Nhập dữ liệu từ người dùng
+print("---  HỆ THỐNG NHẬP CHỈ SỐ SINH TỒN")
+name_patient = input("Nhập tên bệnh nhân: ")
+weight = float(input("Nhập cân nặng bện nhân: "))
+
+# In ra dữ liệu bệnh nhân
+print("---KIỂM TRA DỮ LIỆU LƯU TRỮ---")
+print("Bệnh nhân:",name_patient)
+print("Cân nặng bệnh nhân:",weight)
